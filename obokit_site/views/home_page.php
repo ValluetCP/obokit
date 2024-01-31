27de5b8fcd286263787cdce07e686e0cfb0ea000
Cynthia <cynthia.petitot@gmail.com>
2024-01-31 22:24:29 +0100

    Section 4 & section 5
    Dossier - obokit_site

---
+++ obokit_site/views/home_page.php
@@ -0,0 +1,82 @@
+<?php
+include_once "./inc/header.php";
+include_once "./inc/nav.php";
+?>
+
+<div class="container_section">
+
+    <!-- SECTION 1 -->
+    <section class="section1">
+        
+    </section>
+
+    <!-- SECTION 2 -->
+    <section class="section2">
+
+    </section>
+    
+    <!-- SECTION 3 -->
+    <section class="section3">
+        <h2>Nous vous<br><span>ACCUEILLONS</span></h2>
+        <div class="ouverture">
+            <h3>Jours D'ouvertures</h3>
+            <p>lundi, mardi, jeudi, vendredi et samedi</p>
+        </div>
+        <div class="fermeture">
+            <h3>Jours De Fermetures</h3>
+            <p>mercredi et dimanche</p>
+        </div>
+    </section>
+    
+    <!-- SECTION 4 -->
+    <section class="section4">
+        <h2>Où nous<br><span>TROUVER ?</span></h2>
+        <div class="nous_trouver">
+            <div class="semaine">
+                <div class="jour">LUNDI</div>
+                <div class="heure">18h00-22h00</div>
+                <div class="adresse">Place Léo-lagrange, 94800 Villejuif</div>
+                <div class="plusDetail"><img src="./asset/img/plus_detail.svg" alt=""></div>
+            </div>
+            <hr>
+            <div class="semaine semaine_fond">
+                <div class="jour">MARDI</div>
+                <div class="heure">11h30-15h00<br><span>17h00-22h00</span></div>
+                <div class="adresse">Place Condorcet, centre ville près du café Leffe,<br><span>92340 Bourg la reine</span></div>
+                <div class="plusDetail"><img src="./asset/img/plus_detail.svg" alt=""></div>
+            </div>
+            <hr>
+            <div class="semaine">
+                <div class="jour">JEUDI</div>
+                <div class="heure">11h30-15h00<br><span>17h00-22h00</span></div>
+                <div class="adresse">Place Condorcet, centre ville près du café Leffe,<br><span>92340 Bourg la reine</span></div>
+                <div class="plusDetail"><img src="./asset/img/plus_detail.svg" alt=""></div>
+            </div>
+            <hr>
+            <div class="semaine semaine_fond">
+                <div class="jour">VENDREDI</div>
+                <div class="heure">11h30-22h00</div>
+                <div class="adresse">Place Léo-lagrange, 94800 Villejuif</div>
+                <div class="plusDetail"><img src="./asset/img/plus_detail.svg" alt=""></div>
+            </div>
+            <hr>
+            <div class="semaine">
+                <div class="jour">SAMEDI</div>
+                <div class="heure">11h30-15h00</div>
+                <div class="adresse">Place Condorcet, centre ville près du café Leffe,<br><span>92340 Bourg la reine</span></div>
+                <div class="plusDetail"><img src="./asset/img/plus_detail.svg" alt=""></div>
+            </div>
+        </div>
+    </section>
+
+    <!-- SECTION 5 -->
+    <section  class="section5">
+
+    </section>
+</div>
+
+
+
+<?php
+include_once "./inc/footer.php";
+?>
--- obokit_site/views/inc/header.php
+++ obokit_site/views/inc/header.php
@@ -6,7 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="../asset/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;1,300;1,400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./asset/css/style.css">
     <title>O'bokit</title>
     <!-- Base de donnée : nom de la base de donnée -->
 </head>
