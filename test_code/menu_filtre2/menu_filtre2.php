63c1c6bba03ae62a0d5658827f6181c854574017
CYNTHIA <cynthia.petitot@gmail.com>
2024-02-07 14:49:45 +0100

    Section 5 - Suite mise en page filtre réussi !! (partie 2)
    menu_filtre2.php
    menu_filtre2.css

--- test_code/menu_filtre2/menu_filtre2.php
+++ test_code/menu_filtre2/menu_filtre2.php
@@ -18,7 +18,6 @@
     <script type="text/javascript" src="js/lightbox.js"></script>
     <script type="text/javascript" src="./site.js"></script>
     <script type="text/javascript" src="./accordeon.js"></script>
-    
 </head>
 
 <body>
@@ -62,25 +61,37 @@
                         <li>
                             <a href="#" class="btnChoix" id="viande" title="viande">Viande</a>
                         </li>
+                        <li>
+                            <a href="#" class="btnChoix" id="accompagenement" title="accompagenement">accompagenement</a>
+                        </li>
                     </ul>
                 </div>
 
 
                 <!-- BOWL -->
                 <div class="flex  filtre_ingredient" id="list_bowl">
+                    
                     <figure class="poulet">
                         <div class="fond_noir">
                             <img src="../image/bowl_complet_uber.JPG" alt="">
                         </div>
-                        <h4><span>poulet</span><br>boucané banane plantain</h4>
-                        <p>Sauce chien, sauce cocktail, salade, tomate, oignon rouge, cheddar.</p>
+                        <h4>poulet boucané</h4>
+                        <p>Sauce chien, banane plantain, juliennes de carottes et choux, tomate, oignon rouge, avocat.</p>
+                    </figure>
+                    
+                    <figure class="poulet">
+                        <div class="fond_noir">
+                            <img src="../image/bowl_complet_uber.JPG" alt="">
+                        </div>
+                        <h4>poulet yassa</h4>
+                        <p>Sauce chien, banane plantain, juliennes de carottes et choux, tomate, oignon rouge, avocat.</p>
                     </figure>
 
                     <figure class="viande">
                         <div class="fond_noir">
                             <img src="../image/bowl_complet_uber.JPG" alt="">
                         </div>
-                        <h4><span>complet</span><br>yassa</h4>
+                        <h4>Complet</h4>
                         <p>Salade, tomate, olive, oignon rouge, carotte,moutarde, citron.</p>
                     </figure>
 
@@ -88,16 +99,16 @@
                         <div class="fond_noir">
                             <img src="../image/bowl_morue_uber.JPG" alt="">
                         </div>
-                        <h4><span>MORUE</span><br>CHIQUETAILLE</h4>
-                        <p>Sauce chien, sauce blanche, salade, tomate, oignon rouge, avocat.</p>
+                        <h4>MORUE</h4>
+                        <p>Sauce chien, banane plantain, juliennes de carottes et choux, tomate, oignon rouge, avocat.</p>
                     </figure>
 
                     <figure class="legume">
                         <div class="fond_noir">
                             <img src="../image/bowl_vegan_uber.JPG" alt="">
                         </div>
-                        <h4><span>legume</span><br>boucané banane plantain</h4>
-                        <p>Sauce chien, sauce cocktail, salade, tomate, oignon rouge, cheddar.</p>
+                        <h4>Végétarien</h4>
+                        <p>Sauce chien, mélanges de légumes mariné, banane plantain, juliennes de carottes et choux, tomate, oignon rouge, avocat.</p>
                     </figure>
                 </div>
 
@@ -107,7 +118,7 @@
                         <div class="fond_noir">
                             <img src="../image/bokit_poulet_uber.JPG" alt="">
                         </div>
-                        <h4><span>poulet</span><br>boucané banane plantain</h4>
+                        <h4><span>poulet</span><br>boucané</h4>
                         <p>Sauce chien, sauce cocktail, salade, tomate, oignon rouge, cheddar.</p>
                     </figure>
                     
@@ -125,6 +136,14 @@
                         </div>
                         <h4><span>poulet</span><br>boucané banane plantain</h4>
                         <p>Sauce chien, sauce cocktail, salade, tomate, oignon rouge, cheddar.</p>
+                    </figure>
+
+                    <figure class="poulet">
+                        <div class="fond_noir">
+                            <img src="../image/poulet_b_b.jpg" alt="">
+                        </div>
+                        <h4><span>poulet</span><br>Marinade maison</h4>
+                        <p>Sauce chien, sauce blanche, champignon, salade, tomate, oignon rouge, cheddar.</p>
                     </figure>
 
                     <figure class="poisson">
@@ -147,31 +166,55 @@
                         <div class="fond_noir">
                             <img src="../image/poulet_yassa.jpg" alt="">
                         </div>
-                        <h4><span>legume</span><br>yassa</h4>
-                        <p>Salade, tomate, olive, oignon rouge, carotte,moutarde, citron.</p>
+                        <h4>Végétarien</h4>
+                        <p>Sauce chien, sauce blanche, mélanges de légumes marinés, salade, tomate, oignon rouge.</p>
                     </figure>
 
                     <figure class="viande">
                         <div class="fond_noir">
                             <img src="../image/poulet_yassa.jpg" alt="">
                         </div>
-                        <h4><span>viande</span><br>yassa</h4>
-                        <p>Salade, tomate, olive, oignon rouge, carotte,moutarde, citron.</p>
+                        <h4><span>steak & bacon bœuf</span><br>complet</h4>
+                        <p>Sauce poivre, salade, tomates, oignons, steak, bacon bœuf, œuf et Cheddar.</p>
                     </figure>
                 </div>
                 
                 
                 <!-- PETITE FAIM -->
                 <div class="flex filtre_ingredient"  id="list_petite_faim">
-                    <figure class="petite_faim">
+                    <figure class="poulet">
                         <div class="fond_noir">
                             <img src="../image/Obokit-food-truck_pastel.jpg" alt="">
                         </div>
-                        <h4>Pastel</h4>
-                        <p>Sauce chien, sauce cocktail, salade, tomate, oignon rouge, cheddar.</p>
+                        <h4><span>Pastel</span><br>poulet curry</h4>
+                        <p>filet de blanc de poulet, curry, nokoss, oignons, persil et épices spéciales pour poulet.</p>
                     </figure>
                     
-                    <figure class="petite_faim">
+                    <figure class="viande">
+                        <div class="fond_noir">
+                            <img src="../image/Obokit-food-truck_pastel.jpg" alt="">
+                        </div>
+                        <h4><span>Pastel</span><br>bœuf fromage</h4>
+                        <p>viande hachée, nokoss, persil, oignons, ail, épice grill et cheddar.</p>
+                    </figure>
+
+                    <figure class="crevette">
+                        <div class="fond_noir">
+                            <img src="../image/Obokit-food-truck_pastel.jpg" alt="">
+                        </div>
+                        <h4><span>Pastel</span><br>crevettes </h4>
+                        <p>crevette grise, nokoss, persil, oignons, et épices spéciales pour fruits de mer.</p>
+                    </figure>
+
+                    <figure class="poisson">
+                        <div class="fond_noir">
+                            <img src="../image/Obokit-food-truck_pastel.jpg" alt="">
+                        </div>
+                        <h4><span>Pastel</span><br>saumon </h4>
+                        <p>saumon fumé, ciboulette, fromage, ail et fines herbes, parfait pour une collaboration raffinée.</p>
+                    </figure>
+                    
+                    <figure class="poisson">
                         <div class="fond_noir">
                             <img src="../image/Obokit-food-truck_accras.jpg" alt="">
                         </div>
@@ -184,7 +227,7 @@
                             <img src="../image/frite_pommeDeTerre_uber.JPG" alt="">
                         </div>
                         <h4><span>frite</span><br>pomme de terre</h4>
-                        <p>Sauce chien, sauce cocktail, salade, tomate, oignon rouge, cheddar.</p>
+                        <p>Servi avec une sauce de votre choix.</p>
                     </figure>
 
                     <figure class="accompagenement">
@@ -192,7 +235,7 @@
                             <img src="../image/frite_patateDouce_uber.JPG" alt="">
                         </div>
                         <h4><span>frite</span><br>patate douce</h4>
-                        <p>Sauce blanche, salade, tomate, oignon rouge, avocat, parmesan.</p>
+                        <p>Servi avec une sauce de votre choix.</p>
                     </figure>
 
                     <figure class="accompagenement">
@@ -200,7 +243,7 @@
                             <img src="../image/alloco_uber.JPG" alt="">
                         </div>
                         <h4><span>alloco</span><br>banane plantain</h4>
-                        <p>Sauce chien, sauce blanche, salade, tomate, oignon rouge, avocat.</p>
+                        <p>Servi avec une sauce de votre choix.</p>
                     </figure>
                 </div>
             </div>
@@ -220,6 +263,7 @@
                 $('#list_bokit').show();
                 $('#list_bowl').hide();
                 $('#list_petite_faim').hide();
+                $('#list_menu_secondaire').find('#accompagenement').hide();
             }
 
             // Appel de la fonction pour afficher les éléments requis lors du chargement de la page
@@ -229,28 +273,43 @@
             $('.bowl-link').click(function(e) {
                 e.preventDefault();
                 $('#list_menu_secondaire').find('#poisson,#poulet,#legume,#viande').show();
-                $('#list_menu_secondaire').find('#crevette').hide();
+                $('#list_menu_secondaire').find('#crevette,#accompagenement').hide();
                 $('#list_bowl').show();
                 $('#list_bokit').hide();
                 $('#list_petite_faim').hide();
+                // Réinitialiser l'affichage à 'tout'
+                $('#filtre li').removeClass('active');
+                $('#filtre #tout').addClass('active');
+                $('#list_bowl').find('figure').show();
+                
             });
 
             // Gérer le clic sur le lien "BOKIT"
             $('.bokit-link').click(function(e) {
                 e.preventDefault();
                 $('#list_menu_secondaire').find('.btnChoix').show();
+                $('#list_menu_secondaire').find('#accompagenement').hide();
                 $('#list_bokit').show();
                 $('#list_bowl').hide();
                 $('#list_petite_faim').hide();
+                // Réinitialiser l'affichage à 'tout'
+                $('#filtre li').removeClass('active');
+                $('#filtre #tout').addClass('active');
+                $('#list_bokit').find('figure').show();
             });
 
             // Gérer le clic sur le lien "PETITE FAIM"
             $('.petiteFaim-link').click(function(e) {
                 e.preventDefault();
-                $('#list_menu_secondaire').find('.btnChoix').show();
+                $('#list_menu_secondaire').find('#poisson,#poulet,#crevette,#viande,#accompagenement').show();
+                $('#list_menu_secondaire').find('#legume').hide();
                 $('#list_petite_faim').show();
                 $('#list_bokit').hide();
                 $('#list_bowl').hide();
+                // Réinitialiser l'affichage à 'tout'
+                $('#filtre li').removeClass('active');
+                $('#filtre #tout').addClass('active');
+                $('#list_petite_faim').find('figure').show();
             });
 
 
